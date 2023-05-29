4f42b00e2426aae0431de9e68685d32ff2965980
SeyoungCho <wnl383@naver.com>
2023-05-29 11:47:40 +0000

    feat: add SearchForm Component

---
+++ components/SearchForm.jsx
@@ -0,0 +1,19 @@
+import { useRouter } from "next/router";
+import { useState } from "react";
+export default function SearchForm() {
+  const router = useRouter();
+  const [value, setValue] = useState();
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    router.push(`/search?q=${value}`);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input name="q" value={value} onChange={handleChange} />
+      <button type="submit">검색</button>
+    </form>
+  );
+}
