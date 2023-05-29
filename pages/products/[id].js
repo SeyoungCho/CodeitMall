642f252be43d79e4c5ad63e79f4b5b6a6c9dad9a
SeyoungCho <wnl383@naver.com>
2023-05-29 12:39:08 +0000

    feat: add product detail page

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,7 +1,36 @@
 import { useRouter } from "next/router";
+import instance from "@/lib/axios";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import SizeReviewList from "@/components/SizeReviewList";
 
 export default function Product() {
   const router = useRouter();
   const { id } = router.query;
-  return <div>Product {id}페이지</div>;
+  const [product, setProduct] = useState();
+  const [sizeReviews, setSizeRevies] = useState([]);
+
+  const getProduct = async (targetId) => {
+    const res = await instance.get(`products/${targetId}`);
+    const nextProduct = res.data;
+    setProduct(nextProduct);
+  };
+  const getSizeReviews = async (targetId) => {
+    const res = await instance.get(`size_reviews/?product_id=${targetId}`);
+    const nextSizeReviews = res.data.results ?? [];
+    setSizeRevies(nextSizeReviews);
+  };
+  useEffect(() => {
+    if (!id) return;
+    getProduct(id);
+    getSizeReviews(id);
+  }, [id]);
+  if (!product) return null;
+  return (
+    <div>
+      <h1>{product.name}</h1>
+      <Image src={product.imgUrl} alt={product.name} />
+      <SizeReviewList sizeReviews={sizeReviews} />
+    </div>
+  );
 }
