d06054eeaeff5152e022c41f9c8c3d23612e1f07
SeyoungCho <wnl383@naver.com>
2023-05-30 13:33:40 +0900

    feat: add product list to Homepage

---
+++ components/ProductList.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./ProductList.module.css";
+export default function ProductList({ products = [] }) {
+  return (
+    <ul className={styles.productList}>
+      {products.map((product) => (
+        <li key={product.id}>
+          <Link className={styles.product} href={`/products/${product.id}`} />
+          <Image
+            src={product.imgUrl}
+            width={300}
+            height={300}
+            alt={product.name}
+          />
+          <span className={styles.productName}>{product.name}</span>
+          <br />
+          {product.price}원
+        </li>
+      ))}
+    </ul>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,28 @@
+import SearchForm from "@/components/SearchForm";
 import styles from "@/styles/Home.module.css";
+import ProductList from "@/components/ProductList";
+import instance from "@/lib/axios";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return <h1 className={styles.title}>안녕 Next.js!</h1>;
+  const [products, setProducts] = useState([]);
+  const getProducts = async () => {
+    try {
+      const res = await instance.get("/products");
+      const nextProducts = res.data.results;
+      setProducts(nextProducts);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  useEffect(() => {
+    getProducts();
+  }, []);
+  return (
+    <>
+      <h1>CodeitMall</h1>
+      <SearchForm />
+      <ProductList products={products} />
+    </>
+  );
 }
