65d49ec6a7ba2a07f574d46b518000923d9e0499
SeyoungCho <wnl383@naver.com>
2023-05-30 13:58:48 +0900

    feat: add search feature

--- pages/search.js
+++ pages/search.js
@@ -1,14 +1,32 @@
+import ProductList from "@/components/ProductList";
 import SearchForm from "@/components/SearchForm";
+import instance from "@/lib/axios";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function Search() {
   const router = useRouter();
   const { q } = router.query;
+  const [searchResult, setSearchResult] = useState([]);
+
+  const getSearchResult = async (q) => {
+    try {
+      const res = await instance.get(`/products/?q=${q}`);
+      const results = res.data.results;
+      setSearchResult(results);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  useEffect(() => {
+    getSearchResult(q);
+  }, [q]);
   return (
     <div>
       <h1>Search 페이지</h1>
       <SearchForm initialValue={q} />
       <div>{q} 검색결과</div>
+      <ProductList products={searchResult} />
     </div>
   );
 }
